8dea43831b34bfc373e472e63b82645b1b036ff0
devteam 01 <devteam01@pinnacle.com>
2023-02-13 23:53:08 +1300

    upgraded the user modal

--- src/Modals/OrganizationModal.jsx
+++ src/Modals/OrganizationModal.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Modal, Button } from "react-bootstrap";
 
 export default function OrganizationModal() {
   const [show, setShow] = useState(false);
   const handleclose = () => setShow(false);
   const handleshow = () => setShow(true);
+  const [nameerror, name_error] = useState('');
+  const [emailerror, email_error] = useState('');
+  //const [num_error, numerror] = useState('');
+
 
 
 const initialValues = {
@@ -14,16 +18,78 @@ const initialValues = {
   user : "",
 };
 const[finalORG,Setorg]=useState(initialValues);
+
 const handleInputChange = (e) =>{
-  const {finaldata,value} = e.target;
-  Setorg({...finalORG,[finaldata]:value})
+  const {name,value} = e.target;
+  Setorg({...finalORG,[name]:value})
 }
+const add = () => {
+  alert(finalORG.orgname)
+};
+
+// validation part form form starting
+useEffect(() => {
+  const name_pattern = /[A-Z][A-Z\d]+/;    
+  if (finalORG.orgname === "") 
+  {
+      name_error("");
+  } 
+  else
+   {
+      if (!finalORG.orgname.match(name_pattern)) {
+          name_error("Error found in org name");
+      }
+      else
+       {
+          name_error("");
+      }
+  }
+
+}, [finalORG.orgname])
+
+useEffect(() => {
+  const email_pattern = /^[A-Za-z0-9+_.-]+@(.+)$/;
+
+    if (finalORG.email === "") 
+    {
+        email_error("");
+    } 
+    else
+     {
+        if (!finalORG.email.match(email_pattern)) {
+            email_error("Error found in email");
+        }
+        else
+         {
+            email_error("");
+        }
+    }
+
+}, [finalORG.email])
 
 
+// useEffect(() => {      
+//     const num_patt = /^(0|91)?[6-9][0-9]{9}$/;
+//       if (usernumber === "") 
+//       {
+//           numerror("");
+//       } 
+//       else
+//        {
+//           if (!usernumber.match(num_patt)) {
+//               numerror("Error found in number");
+//           }
+//           else
+//            {
+//               numerror("");
+//           }
+//       }
+  
+//   }, [usernumber])
 
-  const add = () => {
-    alert("done");
-  };
+// validation part ending
+
+ 
 
   return (
     <>
@@ -34,31 +100,34 @@ const handleInputChange = (e) =>{
       >
         Add Organization
       </Button>
-      <Modal show={show} onHide={handleclose} keyboard={false} centered>
+      <Modal show={show} onHide={handleclose} keyboard={true} centered>
         <Modal.Header closeButton></Modal.Header>
         <Modal.Title>Add Organization</Modal.Title>
         <Modal.Body>
           <label>Organization Name : </label>
           <br></br>
           <input 
-          onChange={handleInputChange}
-            value={finalORG.orgname}
+             name="orgname"
+             value={finalORG.orgname}
+            onChange={handleInputChange}
             type="text"
             placeholder="Organization Name"
-          ></input>{" "}
+          ></input>{" "}<p className="bg-danger">{nameerror}</p>
           <br></br>
           <label>Email : </label>
           <br></br>
           <input 
+          name="email"
           onChange={handleInputChange}
            value={finalORG.email}
             type="text"
             placeholder="Email"
-          ></input>{" "}
+          ></input>{" "}<p className="bg-danger">{emailerror}</p>
           <br></br>
           <label>Address : </label>
           <br></br>
           <textarea 
+          name="address"
           onChange={handleInputChange}
           value={finalORG.address}
             rows="5"
@@ -69,6 +138,7 @@ const handleInputChange = (e) =>{
           <label>Number of User : </label>
           <br></br>
           <input  
+          name="user"
           onChange={handleInputChange}
           value={finalORG.user}
           type="text" 
--- src/Modals/UserModal.jsx
+++ src/Modals/UserModal.jsx
@@ -1,13 +1,101 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { Modal, Button} from "react-bootstrap";
 
 export default function UserModal() {
   const [show, setShow] = useState(false);
   const handleclose = () => setShow(false);
   const handleshow = () => setShow(true);
+const[nameerror,SetNameError]=useState('');
+const[emailerror,SetEmailError]=useState('');
+const[numbererror,setNumberError]=useState('');
+const[passworderror,SetPasswordError]=useState('');
+
+const initialUserValues = {
+  name  :"",
+  email : "",
+  password : "",
+  confirmPassword : "",
+  number : "",
+  address : "",
+  OrgBelongsTo : ""
+}
+
+//setting the initial values to the variable finalUserData
+  const[finalUSERdata,setdata]=useState(initialUserValues);
+
+  //setting the values to the finalauserData from every input values which will insert data to the respected variable present in initialUserValues
+  const handleInputChange = (e)=>{
+    const{name,value}=e.target;
+    setdata({...finalUSERdata,[name]:value})
+  }
+
+//validation part starting 
+
+//validation for username
+useEffect(()=>{
+  const name_pattern = /[A-Za-z]+/;   
+  if(finalUSERdata.name == ""){
+    SetNameError("");
+  }
+    else if(!finalUSERdata.name.match(name_pattern))
+     {
+             SetNameError("please Enter Name in Letters")
+    }
+    else{
+      SetNameError("");
+    }
+  },[finalUSERdata.name])
+
+
+  //validation for email
+  useEffect(()=>{
+    const email_pattern = /^[a-za-z0-9+_.-]+@(.+)$/;  
+    if(finalUSERdata.email == ""){
+      SetEmailError("");
+    }
+      else if(!finalUSERdata.email.match(email_pattern))
+       {
+               SetEmailError("please Enter a valid Email")
+      }
+      else{
+        SetEmailError("");
+      }
+    },[finalUSERdata.email])
+
+ //validation for Number
+ useEffect(()=>{
+  const num_patt = /^(0|91)?[6-9][0-9]{9}$/;
+  if(finalUSERdata.number == ""){
+    SetNameError("");
+  }
+    else if(!finalUSERdata.number.match(num_patt))
+     {
+              setNumberError("please Enter a valid Number")
+    }
+    else{
+      setNumberError("");
+    }
+  },[finalUSERdata.number])
+
+  //validation for password
+ useEffect(()=>{
+  const password_pattern = /[0-9]/;
+  if(finalUSERdata.password == ""){
+    SetPasswordError("");
+  }
+    else if(!finalUSERdata.password.match(password_pattern))
+     {
+              SetPasswordError("please Enter a valid Number")
+    }
+    else{
+      SetPasswordError("");
+    }
+  },[finalUSERdata.password])
+
+//validation part ending
 
   const add = () =>{
-    alert("done")
+    alert(finalUSERdata.name)
   }
 
   return (
@@ -27,36 +115,74 @@ export default function UserModal() {
         <Modal.Body>
           <label>User Name : </label>
           <br></br>
-          <input type="text" placeholder="Organization Name"></input> <br></br>
+          <input 
+          name="name"
+          value={finalUSERdata.name}
+          onChange={handleInputChange}
+          type="text" 
+          placeholder="Name"></input> <br></br>
+          <p className="bg-danger">{nameerror}</p>
 
           <label>Email : </label>
           <br></br>
-          <input type="text" placeholder="Email"></input> <br></br>
+          <input 
+           name="email"
+           value={finalUSERdata.email}
+           onChange={handleInputChange}
+          type="text" 
+          placeholder="Email"></input> <br></br>
+           <p className="bg-danger">{emailerror}</p>
 
           <label>Password : </label>
           <br></br>
-          <input type="text" placeholder="Email"></input> <br></br>
+          <input 
+           name="password"
+           value={finalUSERdata.password}
+           onChange={handleInputChange}
+          type="text" 
+          placeholder="Password"></input> <br></br>
+           <p className="bg-danger">{passworderror}</p>
 
           <label> Confirm Password : </label>
           <br></br>
-          <input type="text" placeholder="Email"></input> <br></br>
+          <input
+           name="finalPassword"
+           value={finalUSERdata.confirmPassword}
+           onChange={handleInputChange} 
+          type="text"
+          placeholder="Confirm Password"></input> <br></br>
 
           <label> org belongs </label>
           <br></br>
-          <input type="text" placeholder="Email"></input> <br></br>
+          <input 
+           name="OrgBelongsTo"
+           value={finalUSERdata.OrgBelongsTo}
+           onChange={handleInputChange}
+          type="text" 
+          placeholder=""></input> <br></br>
 
           <label> Phone Number </label>
           <br></br>
-          <input type="text" placeholder="Email"></input> <br></br>
+          <input 
+           name="number"
+           value={finalUSERdata.number}
+           onChange={handleInputChange}
+          type="text" 
+          placeholder="Number"></input> <br></br>
+           <p className="bg-danger">{numbererror}</p>
 
 
           <label>Address : </label>
           <br></br>
           <textarea
+           name="address"
+           value={finalUSERdata.address}
+           onChange={handleInputChange}
             rows="5"
             cols="20"
-            placeholder="Organization Name"
+            placeholder="Address"
           ></textarea>
+
         </Modal.Body>
         <Modal.Footer>
             <Button onClick={handleclose}>Close</Button>
