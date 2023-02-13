679eaa2e077e0302b6dddfd885763b9db30a9ba4
devteam 01 <devteam01@pinnacle.com>
2023-02-13 17:37:32 +1300

    org change

--- src/Modals/OrganizationModal.jsx
+++ src/Modals/OrganizationModal.jsx
@@ -1,59 +1,82 @@
 import React, { useState } from "react";
-import { Modal, Button} from "react-bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 export default function OrganizationModal() {
   const [show, setShow] = useState(false);
   const handleclose = () => setShow(false);
   const handleshow = () => setShow(true);
 
-  const add = () =>{
-    alert("done")
-  }
+
+const initialValues = {
+  orgname : "",
+  email : "",
+  address  : "",
+  user : "",
+};
+const[finalORG,Setorg]=useState(initialValues);
+const handleInputChange = (e) =>{
+  const {finaldata,value} = e.target;
+  Setorg({...finalORG,[finaldata]:value})
+}
+
+
+
+  const add = () => {
+    alert("done");
+  };
 
   return (
     <>
       <Button
         className="btn btn-primary btn-inline"
         type="submit"
-        onClick={handleshow}>
+        onClick={handleshow}
+      >
         Add Organization
       </Button>
-      <Modal show={show}
-       onHide={handleclose} 
-       keyboard={false} 
-       centered>
+      <Modal show={show} onHide={handleclose} keyboard={false} centered>
         <Modal.Header closeButton></Modal.Header>
         <Modal.Title>Add Organization</Modal.Title>
         <Modal.Body>
           <label>Organization Name : </label>
           <br></br>
-          <input  
-          //value={Name}
-          type="text" 
-          placeholder="Organization Name"></input>  <br></br>
-
-          <label>Email : </label>  
+          <input 
+          onChange={handleInputChange}
+            value={finalORG.orgname}
+            type="text"
+            placeholder="Organization Name"
+          ></input>{" "}
+          <br></br>
+          <label>Email : </label>
           <br></br>
           <input 
-         // value={Email}
-          type="text" 
-          placeholder="Email"></input>    <br></br>
-
+          onChange={handleInputChange}
+           value={finalORG.email}
+            type="text"
+            placeholder="Email"
+          ></input>{" "}
+          <br></br>
           <label>Address : </label>
           <br></br>
-          <textarea
+          <textarea 
+          onChange={handleInputChange}
+          value={finalORG.address}
             rows="5"
             cols="20"
             placeholder="Organization Name"
-          ></textarea><br></br>
-
+          ></textarea>
+          <br></br>
           <label>Number of User : </label>
           <br></br>
-          <input type="text" placeholder=""></input>
+          <input  
+          onChange={handleInputChange}
+          value={finalORG.user}
+          type="text" 
+          placeholder=""></input>
         </Modal.Body>
         <Modal.Footer>
-            <Button onClick={handleclose}>Close</Button>
-            <Button onClick={add}></Button>
+          <Button onClick={handleclose}>Close</Button>
+          <Button onClick={add}></Button>
         </Modal.Footer>
       </Modal>
     </>
